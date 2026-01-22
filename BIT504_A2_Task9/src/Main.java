f5713bfd03b5562f9fc9598926426a9ebf4ba5e0
JohnWardNZ <johnwardnz@outlook.com>
2026-01-22 15:10:01 +1300

    Added additional console message for branch 1

--- BIT504_A2_Task9/src/Main.java
+++ BIT504_A2_Task9/src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Task 9: Version control demo");
         System.out.println("This is my first commit.");
+        System.out.println("Feature branch 1 active");
     }
 }
